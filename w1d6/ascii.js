76f776780d9d14b11f85fa6cdd18eaba87f4b2bb
Abebe Tekle Abrha <abe.habesha@gmail.com>
2021-07-26 01:06:12 -0500

    Update ascii.js

--- w1d6/ascii.js
+++ w1d6/ascii.js
@@ -6,91 +6,63 @@ let beginningText="";
 let turboClicked = false;
 
 window.onload = function () {
-    "use strict";
-    
+    "use strict";    
     // start button
     let startButton = window.document.getElementById("start");
-    startButton.onclick = function () { startFunction("nn") };
-
+    startButton.onclick = function () { startFunction("begin") };
     // animation type
     let animationType = document.getElementById("animation");
     animationType.onchange = function () { animationSelector(animationType.value) };
-
-
     // fontsize
     let fontsize = document.getElementById("fontsize");
     fontsize.onchange = function () { changeFontSize(fontsize.value) };
-
     // stop button 
     let stopBtn = document.getElementById("stop");
     stopBtn.onclick = function () { stop() };
-
     // turbo - speed
     let turbo = document.getElementById("turbo");
     turbo.onclick = function () { turboF() };
-
 }
-
 function startFunction(stringValue) {
-
-
     let textArea = document.getElementById("text-area");
     let textAreaValue;
-
-    if (stringValue !== "nn") {
+    if (stringValue !== "begin") {
         textAreaValue = stringValue;
         beginningText = textAreaValue;
     }
     else {
-
         textAreaValue = textArea.innerHTML;
         beginningText = textAreaValue;
         if (textAreaValue === "") {
             textArea.innerHTML = "Warning: choose animation type!!!";
             return;
-
         }
     }
     document.getElementById("stop").disabled = false;
     document.getElementById("start").disabled = true;
     document.getElementById("animation").disabled = true;
-
     const splitted = textAreaValue.split("=====\n");
     let i = 0;
     id = setInterval(setAnimation, interval)
     function setAnimation() {
-        if (i === splitted.length) {
+        if (i === splitted.length) 
             i = 0;
-            textArea.innerHTML = splitted[i];
-        }
-        else {
-            textArea.innerHTML = splitted[i];
-        }
+        textArea.innerHTML = splitted[i];        
         i++;
-
     }
-
 }
 function stop() {
     document.getElementById("start").disabled = false;
-    document.getElementById("animation").disabled = false;
-    
+    document.getElementById("animation").disabled = false;    
     clearInterval(id);
     if(turboClicked){
         document.getElementById("text-area").innerHTML = beginningText.split("=====\n")[0];
-        turboClicked = false;
-        
+        turboClicked = false;        
     }
-    else{
+    else
         document.getElementById("text-area").innerHTML = beginningText;
-
-    }
-
-    
     document.getElementById("stop").disabled = true;
-    // $("#stop").disabled = true;
 }
-
 function animationSelector(s) {
     document.getElementById("start").disabled = false;
     stop();
@@ -98,24 +70,17 @@ function animationSelector(s) {
     let textArea = document.getElementById("text-area");
     textArea.innerHTML = ANIMATIONS[whichOne];
 }
-
 function changeFontSize(fontsize) {
     let fontsizeMap = { "Tiny": "7pt", "Small": "10pt", "Medium": "12pt", "Large": "16pt", "Extra Large": "24pt", "XXL": "32pt" };
-
     let textArea = document.getElementById("text-area");
     textArea.style.fontSize = fontsizeMap[fontsize];
-
 }
 function turboF() {
     turbobox = document.getElementById("turbo").checked;
-    if (turbobox === true) {
+    if (turbobox === true) 
         interval = 50;
-    }
-    else {
-        interval = 250;
-    }
-
-    // animationSelector(document.getElementById("animation").value);
+    else 
+        interval = 250;    
     turboClicked = true;
     stop();
     startFunction(ANIMATIONS[document.getElementById("animation").value]);
